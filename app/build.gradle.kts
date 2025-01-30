7999a52d0b2c4f2fbf50ca786f2f1f2d90fc5dce
JEANMONNET\cao_johnny <saracaoying@gmail.com>
2025-01-30 09:55:34 +0100

    creata PaginaClasse

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,6 +26,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -33,9 +36,11 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.4.0") // Compatible with API 16
     implementation("com.google.android.material:material:1.3.0") // Compatible with API 16
     implementation("androidx.constraintlayout:constraintlayout:2.1.4") // Compatible with API 16
+    implementation("androidx.fragment:fragment:1.3.6") // Add Fragment support
 
     // Test dependencies (no changes needed)
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
+
--- app/src/main/java/com/cao/classtrack/MainActivity.java
+++ app/src/main/java/com/cao/classtrack/MainActivity.java
@@ -1,6 +1,10 @@
 package com.cao.classtrack;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -10,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            loadFragment(new PaginaClasse());
+        }
     }
+    private void loadFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragmentContainerView, fragment);
+        fragmentTransaction.commit();
+    }
+
 }
